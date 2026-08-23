42dc05120854a576f6fc794fab750e60a4016fd7
999nowa <131688612+999nowa@users.noreply.github.com>
2026-08-23 19:08:24 +0200

    Fix appens aktiva RouteMapper-skärm

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import RouteMapperFeatureScreen from './src/RouteMapperFeatureScreen';
+import RouteMapperScreen from './src/RouteMapperScreen';
 
 export default function App() {
-  return <SafeAreaProvider><StatusBar barStyle="dark-content" /><RouteMapperFeatureScreen /></SafeAreaProvider>;
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle="light-content" />
+      <RouteMapperScreen />
+    </SafeAreaProvider>
+  );
 }
